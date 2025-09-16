0c07faea8f08652508c1cc380d41a84920254d3a
Jaylord <mazojaylord@gmail.com>
2025-09-16 14:35:27 +0800

    fix: update base_url in config.php to correct live URL for routing fix

--- scheme/kernel/LavaLust.php
+++ scheme/kernel/LavaLust.php
@@ -156,7 +156,7 @@ function &lava_instance()
 $performance->stop('lavalust');
 
 // Handle the request
-$url = $router->sanitize_url(str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['PHP_SELF']));
+$url = $router->sanitize_url(str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['REQUEST_URI']));
 $method = isset($_SERVER['REQUEST_METHOD']) ? strtoupper($_SERVER['REQUEST_METHOD']) : '';
 $router->initiate($url, $method);
 ?>
